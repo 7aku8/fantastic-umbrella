35a44fe3ad9cf85b2d72bbc7ef769fd05e20023e
JakubWolak <jakubwolak.123@gmail.com>
2022-07-03 11:34:38 +0200

    amplify cognito auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "gymtrackerappd7b0df5b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
